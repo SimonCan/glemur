9507eac00e2201c3f998b4523c41e77405af98a5
iomsn <iomsn@a523>
2015-02-05 14:56:30 +0000

    Added initial condition for braids+domes and k=0.35.

--- src/blobsDomes.h
+++ src/blobsDomes.h
@@ -7,6 +7,9 @@
 int initBlobsDomes(struct varsHost_t h, struct parameters_t p);
 
 // initialize the blobsDomeSingle configuration
+int initBlobsDomesK035(struct varsHost_t h, struct parameters_t p);
+
+// initialize the blobsDomeSingle configuration
 int initBlobsDomeSingle(struct varsHost_t h, struct parameters_t p);
 
 #endif /* BLOBSDOMES_H_ */
